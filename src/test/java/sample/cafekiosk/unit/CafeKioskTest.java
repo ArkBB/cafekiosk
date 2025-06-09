9f7e274b39c2a2bf843667757ce78bc46861a5db
pbh914 <52387364+ArkBB@users.noreply.github.com>
2025-06-09 11:42:44 +0900

    수동 테스트
    println으로 찍어보며 눈으로 확인하는 코드

---
+++ src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -0,0 +1,34 @@
+package sample.cafekiosk.unit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import sample.cafekiosk.unit.beverage.Americano;
+
+class CafeKioskTest {
+
+    @Test
+    void add() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        cafeKiosk.add(new Americano());
+
+        System.out.println(">>> 담긴 음료 수 : " + cafeKiosk.getBeverages().size());
+        System.out.println(">>> 담긴 음료 : " + cafeKiosk.getBeverages().get(0).getName());
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void calculateTotalPrice() {
+    }
+
+    @Test
+    void createOrder() {
+    }
+}
